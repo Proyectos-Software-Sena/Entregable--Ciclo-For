08dac81344cd577f409f611e34768f579d6475f9
Sebastian soto <juancarloshj25@gmail.com>
2026-07-24 18:22:03 -0500

    actividad 1 resuelta - cierre de caja

--- actividades/src/Act_1.java
+++ actividades/src/Act_1.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class Act_1 {
+    public static void main (String[] args){
+
+        int compra = Integer.parseInt(JOptionPane.showInputDialog("cuantas compras hubo en el dia"));
+        double suma = 0;
+        for (int i = 1; i <= compra; i++){
+            double precio = Double.parseDouble(JOptionPane.showInputDialog("cunato fue la compre " + i));
+            suma += precio;
+
+        }
+        System.out.println("la venta total fue: " + suma);
+    }
 }
