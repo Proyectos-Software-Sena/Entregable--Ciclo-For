82a1b033cabd3bf1406f144ca25ed5427268293d
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-07-25 13:48:12 -0500

    feat: resolver ejercicio - 8

---
+++ src/src/Ejercicio8.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Ejercicio8 {
+    public static void main(String[] args){
+
+        String palabra;
+        String invertida = "";
+
+        palabra = JOptionPane.showInputDialog("Ingrese una palabra o cadena de texto:");
+
+        for (int i = palabra.length() - 1; i >=0; i--){
+            invertida = invertida + palabra.charAt(i);
+        }
+        JOptionPane.showMessageDialog(null,"Cadena invertida:" + invertida);
+    }
+}
