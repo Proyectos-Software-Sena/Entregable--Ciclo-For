86f1edd7f4145a82329dafe24a167ca0e2af520e
Angie Valentina Buitrago Montes <montesvalen373@gmail.com>
2026-07-26 13:32:59 -0500

    feat: resuelve ejercico 1 - calculo ventas

---
+++ src/Ejercico1.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Ejercico1 {
+    public static void main (String[] args){
+        //ver total de numero recaudado en el dia
+
+        int cantidadVentas = Integer.parseInt(JOptionPane.showInputDialog("cuantas ventas se realizaron hoy?"));
+        double total = 0;
+
+        for (int i= 0; i < cantidadVentas; i++){
+            double ventas = Double.parseDouble(JOptionPane.showInputDialog("ingrese el valorde la venta " + i + ":"));
+            total = total + ventas ;
+        }
+        JOptionPane.showMessageDialog(null,"el total de ventas del dia es :" + total);
+    }
+}
