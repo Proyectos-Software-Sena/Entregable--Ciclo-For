959d5730459a1d196730da47d228d6ea64af7b12
Jhon Anderson Rubiano Monroy <ttrolling182@gmail.com>
2026-07-26 15:41:23 -0500

    feat: resuelve ejercicio 6 - clasificador de edades

---
+++ CiclosForDoWhile/src/Ejercicio6.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args){
+
+        int cantidad;
+        int edad;
+        int menores = 0;
+        int mayores = 0;
+
+        cantidad = Integer.parseInt(JOptionPane.showInputDialog("Cuantos asistentes hay?"));
+
+        for (int i = 1; i <= cantidad; i++){
+
+            edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad del asistente: " + i));
+
+            if (edad < 18){
+
+                menores++;
+            }else{
+                mayores++;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null , "Menores de edad: " + menores + "\nMayores de edad: " + mayores);
+    }
+}
