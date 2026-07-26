4554998c5c796392be96d2354b7733878a27ee7e
Saray Ruiz <ruizsaraygomez.13@gmail.com>
2026-07-26 14:11:33 -0500

    feat: resuelve ejercicio 5 - Promedio de Calificaciones ADSO

---
+++ do-while/src/Ejercicio5.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+
+        double montoAhorro, sumaAhorro;
+        int meses, contador;
+
+        contador = 1;
+        sumaAhorro = 0;
+
+
+        meses = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de meses de la proyección"));
+        montoAhorro = Double.parseDouble(JOptionPane.showInputDialog("Ingrese monto fijo a ahorrar cada mes: "));
+        do {
+            sumaAhorro = sumaAhorro + montoAhorro;
+            contador++;
+
+            JOptionPane.showMessageDialog(null,"Mes" + contador + ": $" + sumaAhorro);
+
+        }while (contador <= meses);
+    }
+}
