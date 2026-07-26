3931298dfe86953dd5bad3a73baff79f0a60b9fb
Leidy Bravo <leidymaireth5555@gmail.com>
2026-07-26 15:33:54 -0500

    Se actualizo el ejercicio 6 con el codigo completo

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Ejercicio6 {
+    public static void main(String[] args) {
+
+        int asistentes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de asistentes: "));
+        int adultos = 0;
+        int menores = 0;
+
+        for (int i = 1; i <= asistentes; i++) {
+            int edad =  Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad del asistente " + i + " :"));
+
+            if (edad >= 18 ) {
+                adultos++;
+            }else {
+                menores++;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Menores : " + menores + "\nAdultos : " + adultos );
+    }
 }
