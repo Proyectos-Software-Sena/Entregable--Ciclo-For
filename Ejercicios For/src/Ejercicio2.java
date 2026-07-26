84448f616d3a5fcdaa7294651b5c10ef2bc1a30b
wilmar martinez <martinezwilmar313@gmail.com>
2026-07-25 22:03:18 -0500

    feat: se realizo el ejercicio 2 - control de calidad

--- Ejercicios For/src/Ejercicio2.java
+++ Ejercicios For/src/Ejercicio2.java
@@ -1,3 +1,22 @@
+import javax.swing.*;
+
 public class Ejercicio2 {
-    
+    public static void main(String[] args){
+
+        int piezasT = Integer.parseInt(JOptionPane.showInputDialog("cuantas piezas va a revisar? "));
+        int bien = 0;
+        int mal = 0;
+
+        for (int i = 1; i <= piezasT; i++){
+
+            int peso = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el peso de cada pieza: "));
+
+            if (peso > 100 && peso < 120) {
+                bien++;
+            }else {
+                mal++;
+            }
+        }
+        JOptionPane.showMessageDialog(null,"piezas aprobadas: " + bien + " piezas defectuosas: " + mal);
+    }
 }
