5757b0b700f2bf6d022694b5704a69f3e27a405b
Saray Ruiz <ruizsaraygomez.13@gmail.com>
2026-07-26 10:44:12 -0500

    feat: resuelve ejercicio 2 - Control de Calidad en Producción

---
+++ do-while/src/Ejercicio2.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        int cantidadPiezas, pesoPiezas, aprobadas, defectuosas, contador;
+
+        cantidadPiezas = Integer.parseInt(JOptionPane.showInputDialog("ingrese la cantidad de piezas a revisar: "));
+        pesoPiezas = 0;
+        aprobadas = 0;
+        defectuosas = 0;
+        contador = 1;
+
+        do {
+
+            pesoPiezas = Integer.parseInt(JOptionPane.showInputDialog("ingrese el peso de la pieza: " + contador));
+
+            if (pesoPiezas < 100 || pesoPiezas > 120) {
+                defectuosas++;
+
+            } else {
+                aprobadas++;
+
+            }
+            contador++;
+
+        } while (contador <= cantidadPiezas);
+        JOptionPane.showMessageDialog(null, "Piezas aprobadas: " + aprobadas + " Piezas defectuosas: " + defectuosas);
+
+    }
+}
