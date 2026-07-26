1e980d4bed1ea34d7d99fdff27d5a039566f027c
Nicol restrepo <nikidayana3@mail.com>
2026-07-26 02:37:30 -0500

    se realizo ejercicio8

---
+++ Ejercicios-Ciclos/src/Ejercicio8.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+
+        String palabra = JOptionPane.showInputDialog("Ingrese una palabra");
+        String invertida = "";
+
+        for (int i = palabra.length() - 1; i >= 0; i--) {
+
+            invertida = invertida + palabra.charAt(i);
+
+        }
+
+        JOptionPane.showMessageDialog(null, "Palabra invertida: " + invertida);
+    }
+}
