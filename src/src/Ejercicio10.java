9f0a8d759768968b636436465af9becf09af95d5
user.name <valerymunozmorales@gmaiil.com>
2026-07-26 16:56:03 -0500

    feat: Resolvi el ejercicio 10 - Estimación Ágil (Sucesión de Fibonacci)

--- src/src/Ejercicio10.java
+++ src/src/Ejercicio10.java
@@ -3,12 +3,21 @@ import javax.swing.*;
 public class Ejercicio10 {
     public static void main (String[] args){
 
-        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de términos"));
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de términos:"));
         int a = 0;
         int b = 1;
+        int siguiente;
+        String mensaje = "";
 
-        for ( int i =1; i <= cantidad; i++){
+        for ( int i =1; i <= cantidad; i++) {
+
+            mensaje = mensaje + a + ", ";
+
+            siguiente = a + b;
+            a = b;
+            b = siguiente;
+        }
+        JOptionPane.showMessageDialog(null, "Serie de Fibonacci: \n" + mensaje);
 
         }
     }
-}
