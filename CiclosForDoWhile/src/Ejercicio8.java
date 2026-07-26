bd05fc30d85c362f12f3d078aaad769e74ff4c43
Jhon Anderson Rubiano Monroy <ttrolling182@gmail.com>
2026-07-26 16:01:48 -0500

    feat: resuelve ejercicio 8 - Invertir una Cadena

---
+++ CiclosForDoWhile/src/Ejercicio8.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Ejercicio8 {
+
+    public static void main(String[] args){
+
+        String palabra;
+        String invertida = "";
+
+        palabra = JOptionPane.showInputDialog("Ingrese una palabra: ");
+
+        for (int i = palabra.length() - 1; i >= 0; i--){
+
+            invertida = invertida + palabra.charAt(i);
+        }
+
+        JOptionPane.showMessageDialog(null ,  "Cadena Invertida: " + invertida);
+    }
+}
