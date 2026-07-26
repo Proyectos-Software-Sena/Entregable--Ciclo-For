4748649055d0d57da3055a2e81475c027cf92715
Saray Ruiz <ruizsaraygomez.13@gmail.com>
2026-07-26 10:53:29 -0500

    feat: resuelve ejercicio 1 - cierre de caja

--- do-while/src/Ejercicio1.java
+++ do-while/src/Ejercicio1.java
@@ -2,21 +2,22 @@ import javax.swing.*;
 
 public class Ejercicio1 {
     public static void main( String[] args) {
-        
-        int cantidadVentas,venta, totalVentas, contador;
 
-        totalVentas = 0;
+        int cantidadVentas,venta, total, contador;
+
+        total = 0;
         contador= 1;
 
         cantidadVentas = Integer.parseInt(JOptionPane.showInputDialog("ingrese la cantidad de vetsas que hubo en el dia: "));
 
         do{
             venta= Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la venta: "));
-            totalVentas = totalVentas + venta;
+            total = total + venta;
             contador++;
         }while (contador <= cantidadVentas);
 
-        JOptionPane.showMessageDialog(null, "El total de ventas del día es: " + totalVentas);
+        JOptionPane.showMessageDialog(null, "El total de ventas del día es: " + total);
 
     }
 }
+
