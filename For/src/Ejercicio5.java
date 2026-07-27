c3c23e6c9d212e2d3c83dc79aec617b45bd3f1df
jacobo cl <camachojuanjacobo@gmail.com>
2026-07-27 04:28:59 -0500

    se hizo el ejercicio5:en el cual se hizo para tener una proyeccion en mis ahorros

---
+++ For/src/Ejercicio5.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+
+        int ahorro = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el monto fijo a ahorrar cada mes"));
+        int meses = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de meses de la proyeccion"));
+
+        int acumulado = 0;
+        String resultado = " PROYECCION DE AHORROS \n\n";
+
+        for(int mes = 1; mes <= meses; mes++){
+            acumulado = acumulado + ahorro; 
+            resultado = resultado + "Mes " + mes + ": $" + acumulado + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, resultado);
+    }
+}
