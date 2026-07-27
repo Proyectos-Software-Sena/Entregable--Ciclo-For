ffadc79814bf45f2def557b7a00f5cf539c4d1ee
Gabriela Uribe <gabrielauribe484@gmail.com>
2026-07-27 14:42:27 -0500

    Se realizó ejercicio 10 - fibonacci

---
+++ EjercicioFor/src/Ejercicio10.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Ejercicio10 {
+    public static void main(String[] args){
+        int n;
+        int a = 0;
+        int b = 1;
+        int siguiente;
+        String resultado = "Serie de Fibonacci:\n";
+        n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de términos: "));
+        for (int i = 1; i <= n; i++){
+            resultado += a + "";
+            siguiente = a + b;
+            a = b;
+            b = siguiente;
+        }
+        JOptionPane.showMessageDialog(null, resultado);
+    }
+}
