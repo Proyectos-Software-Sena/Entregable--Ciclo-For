bcc44c7a95148aa73d8e5a76afcc9d93421e1567
Alejandro Torres <alejandrotorressanchez559@gmail.com>
2026-07-27 08:50:33 -0500

    Resuelvo ejercicio2

--- src/src/ejercicio2.java
+++ src/src/ejercicio2.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
 public class ejercicio2 {
+    public static void main(String[] args) {
+        int numPiezas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de piezas"));
+        int aprobadas = 0, reprobadas = 0;
+
+        for (int i = 1; i <= numPiezas; i++) {
+            double peso = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso (gramos)"));
+            if (peso < 100 || peso > 120){
+                aprobadas++;
+            }
+            else {
+                reprobadas++;
+            }
+        }
+        System.out.println("Piezas aprobadas: " + aprobadas +
+                "\n"
+                +
+                "Piezas reprobadas: " + reprobadas);
+    }
 }
