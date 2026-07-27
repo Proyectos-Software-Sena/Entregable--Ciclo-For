4031d719c9462549ea5da50ac7139177dceb07bc
JCastagu <juanes121208@gmail.com>
2026-07-27 08:01:05 -0500

    Realice el ejercicio 8: Generador de Códigos (Inversor de Cadenas)

---
+++ src/src/Ejercicio8.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Ejercicio8 {
+    public static void main(String[] args){
+        String palabra = JOptionPane.showInputDialog("Ingrese la palabra clave:");
+        String invertida = "";
+
+        for (int i = palabra.length() - 1; i >= 0; i--) {
+            invertida = invertida + palabra.charAt(i);
+        }
+
+        JOptionPane.showMessageDialog(null, "Código generado: " + invertida);
+    }
+}
