4b5ecc5ba7bf6dea2d2f10e593cdd3994febff79
Nicolle Moreno <morenosancheznicollegabriela@gmail.com>
2026-07-27 08:45:34 -0500

    Solucion del ejercicio 4

---
+++ src/Ejercicio4.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        int cantidad;
+        double nota;
+        double suma = 0;
+        double promedio;
+
+        cantidad = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantas calificaciones desea ingresar?"));
+        for (int i = 1; i <= cantidad; i++) {
+            nota = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota" + i + ":"));
+            suma += nota;
+        }
+        promedio = suma / cantidad;
+
+        if (promedio >= 3.0) {
+            JOptionPane.showMessageDialog(null, "Promedio:" + promedio + "\nEstado: Aprobado ");
+        } else {
+            JOptionPane.showMessageDialog(null, "Promedio:" + promedio + "\nEstado: Aprobado ");
+        }
+    }
+}
+
