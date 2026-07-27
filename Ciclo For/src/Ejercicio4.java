5257db2304c6c5365c467409b56af5926247720f
juliana parra <juliparra481@gmail.com>
2026-07-27 18:58:17 -0500

    guarde el ejercicio4

--- Ciclo For/src/Ejercicio4.java
+++ Ciclo For/src/Ejercicio4.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+
 public class Ejercicio4 {
+    public static void main(String[] args) {
+        int modulo = Integer.parseInt(JOptionPane.showInputDialog("ingrese el numero de modulos"));
+        double promedio = 0;
+        double suma = 0;
+        for (int i = 1; i <= modulo; i++) {
+            double notas = Double.parseDouble(JOptionPane.showInputDialog("ingrese las notas "));
+            suma = suma + notas;
+
+        }
+        promedio = suma / modulo;
+
+        if (promedio >= 3.0) {
+            JOptionPane.showMessageDialog(null, "Aprobado, el promedio es de:" + promedio);
+        } else {
+            JOptionPane.showMessageDialog(null, " reprobado el promedio es de:" + promedio);
+        }
+    }
 }
