92cf02ddb8ebd51c5cbfad5a1f8492b6d0f7cefc
juliana parra <juliparra481@gmail.com>
2026-07-27 17:46:19 -0500

    guarde el ejercicio3

--- Ciclo For/src/Ejercicio2.java
+++ Ciclo For/src/Ejercicio2.java
@@ -1,2 +1,18 @@
+import javax.swing.*;
+
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("ingrese la cantidad de piezas"));
+        int defectuosas = 0;
+        int buenas = 0;
+        for (int i = 1; i <= cantidad; i++) {
+            int peso = Integer.parseInt(JOptionPane.showInputDialog("ingrese el peso de la pieza"));
+            if (peso <= 100 || peso >= 120) {
+                defectuosas++;
+            } else {
+                buenas++;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "numero de piezas buenas"+ buenas + "numero de piezas defectuosas"+ defectuosas);
+    }
 }
