0ba37128b4c6a005dacd0eef8afa08492662f8c8
juliana parra <juliparra481@gmail.com>
2026-07-27 18:02:08 -0500

    guarde el ejercicio4

--- Ciclo For/src/Ejercicio3.java
+++ Ciclo For/src/Ejercicio3.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+import java.util.jar.JarOutputStream;
+
 public class Ejercicio3 {
+public static void main(String[] args){
+    int cantidad = Integer.parseInt(JOptionPane.showInputDialog("ingrese la cantidad de lecturas de temperatura que se tomaron"));
+    int mayor = 0;
+    for(int i = 1; i <= cantidad; i++ ){
+        int grados = Integer.parseInt(JOptionPane.showInputDialog("ingrese los grados"));
+
+
+        if (i == 1){
+            mayor = grados;
+        } else if (grados>mayor) {
+            mayor = grados;
+        }
+    }
+    JOptionPane.showMessageDialog(null,"la mayor temperatura fue: " + mayor);
+}
 }
