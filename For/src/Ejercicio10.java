edffb28732c4327f8cf8a64943ff8b7fcce4668e
jacobo cl <camachojuanjacobo@gmail.com>
2026-07-27 17:17:37 -0500

    se hizo el ejercicio10: en el cual trata sobre la cantidad de trminos de la serie de fibonacci

--- For/src/Ejercicio10.java
+++ For/src/Ejercicio10.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import javax.swing.JOptionPane;
 
 public class Ejercicio10 {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de terminos N de la serie de Fibonacci "));
+
+        int a = 0;
+        int b = 1;
+        String serie = "";
+
+        for(int i = 1; i <= n; i++){
+            serie = serie + a;
+            if(i < n){
+                serie = serie + ", ";
+            }
+
+            int siguiente = a + b;
+            a = b; // mover
+            b = siguiente;
+        }
+
+        JOptionPane.showMessageDialog(null, "Serie de Fibonacci con " + n + " terminos:\n" + serie);
+    }
 }
