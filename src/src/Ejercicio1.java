7ef16cb5b4eaeda7c966383238bf4867fbb78ffb
JCastagu <juanes121208@gmail.com>
2026-07-26 23:31:51 -0500

    Realice el Ejercicio numero 1: Cierre de caja

--- src/src/Ejercicio1.java
+++ src/src/Ejercicio1.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Ejercicio1 {
+    public static void main(String[] args){
+        int cantidadVentas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número total de cobros realizados:"));
+        double totalIngresos = 0;
+
+        for (int i = 1; i <= cantidadVentas; i++) {
+            double valorVenta = Double.parseDouble(JOptionPane.showInputDialog("Monto de la compra " + i + ":"));
+            totalIngresos = totalIngresos + valorVenta;
+        }
+
+        JOptionPane.showMessageDialog(null, "Recaudo total de la jornada: $" + totalIngresos);
+
+    }
 }
