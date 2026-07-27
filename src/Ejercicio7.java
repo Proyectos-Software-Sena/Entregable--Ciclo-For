de5597578738aa7f71f5e2fb4d2be5d731dff64c
Daniel Santiago Botina Amortegui <santybotina130921@gmail.com>
2026-07-27 08:31:44 -0500

    ejercicio 7 de los 10

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -4,7 +4,7 @@ public class Ejercicio7 {
 
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Ingrese la Tasa de Cambio Representativa del Mercado (TRM): ");
+            System.out.print("Ingrese la Tasa de Cambio Representativa del Mercado (TRM):");
             double trm = scanner.nextDouble();
 
             for (int usd = 1; usd <= 10; usd++) {
