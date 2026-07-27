a07c87e1af47d424b1cd593b8e334e935007a384
Nicolle Moreno <morenosancheznicollegabriela@gmail.com>
2026-07-27 16:56:51 -0500

    Solucion del ejercicio 8

---
+++ src/Ejercicio8.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Ejercicio8 {
+    public static void main (String[] args){
+        String palabra;
+        String invertida = "";
+        palabra = JOptionPane.showInputDialog("Ingrese una palabra:");
+
+        for (int i = palabra.length()-1; i>=0; i--){
+            invertida += palabra.charAt(i);
+        }
+        JOptionPane.showMessageDialog(null,"Cadena invertida:" + invertida);
+    }
+}
