bb1c50b0b5399be32885080774f663e496d3e262
jacobo cl <camachojuanjacobo@gmail.com>
2026-07-26 22:48:46 -0500

    ejercicio3: fue un medidor de temperatura

--- For/src/Ejercicio3.java
+++ For/src/Ejercicio3.java
@@ -1,2 +1,19 @@
+import javax.swing.JOptionPane;
+
 public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Cuantas temperaturas se tomaron?"));
+        double tempMaxima = -999;
+        for(int i = 1; i <= cantidad; i++){
+            double temperatura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la temperatura " + i + " en °C"));
+            if(temperatura > tempMaxima){
+                tempMaxima = temperatura;
+            }
+        }
+
+        String resultado = "La temperatura maxima registrada fue " + tempMaxima + "°C";
+
+        JOptionPane.showMessageDialog(null, resultado);
+    }
 }
