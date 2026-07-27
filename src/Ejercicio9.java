81eb4f8be70138c9c6ec24a938d0ef844d8fd245
Nicolle Moreno <morenosancheznicollegabriela@gmail.com>
2026-07-27 17:15:02 -0500

    Solucion del ejercicio 9

---
+++ src/Ejercicio9.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Ejercicio9 {
+    public static void main (String[] args) {
+        int bacterias;
+        int horas;
+        String resultado = "";
+
+        bacterias = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de bacterias:"));
+        horas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de horas"));
+
+        for (int i = 1; i <= horas; i++){
+            bacterias = bacterias * 2;
+            resultado += "Hora" + i + ":" + bacterias + "bacterias\n";
+        }
+        JOptionPane.showMessageDialog(null, resultado);
+    }
+}
