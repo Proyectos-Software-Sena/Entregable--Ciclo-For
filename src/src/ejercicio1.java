818795d9728af2a31695ea40d66719773c05794c
Alejandro Torres <alejandrotorressanchez559@gmail.com>
2026-07-27 08:25:33 -0500

    Resuelvo ejercicio1

--- src/src/ejercicio1.java
+++ src/src/ejercicio1.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class ejercicio1 {
+    public static void main(String[] args) {
+        int numeroVentas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de ventas"));
+        int total = 0;
+
+        for (int i = 1; i <= numeroVentas; i++) {
+            System.out.print("Ingrese el valor de la venta " + i + ": ");
+            int valorVenta = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor de la venta "));
+            total += valorVenta;
+        }
+        System.out.println("El total de ventas del día es: $" + total);
+    }
 }
