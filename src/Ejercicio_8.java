755fb05d4fa5ab246350bbaca07d2ae5b16e635c
karen chaparro <ckarendaniela133@gmail.com>
2026-07-27 09:43:05 -0500

    solucion del Ejercicio_8

---
+++ src/Ejercicio_8.java
@@ -0,0 +1,10 @@
+public class Ejercicio_8 {
+    public static void main(String[] args){
+        String cadena = "sena";
+        String invertido = "";
+        for (int i = cadena.length() -1 ; i >= 0; i--){
+            invertido += cadena.charAt(i);
+        }
+        System.out.println(invertido);
+    }
+}
