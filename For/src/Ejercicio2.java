6919562feec71233191317194130b85f167722d4
jacobo cl <camachojuanjacobo@gmail.com>
2026-07-26 21:08:22 -0500

    Se hizo el ejercicio2: control de calidad por peso

---
+++ For/src/Ejercicio2.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        int aprobadas = 0;
+        int defectuosas = 0;
+
+
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Cuántas piezas se van a revisar?"));
+
+        for(int i = 1; i <= cantidad; i++){
+            int peso = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el peso de la pieza " + i + " en gramos"));
+
+            if(peso < 100 || peso > 120){
+                defectuosas++;
+            } else {
+                aprobadas++;
+            }
+        }
+
+        String resultado = "CONTROL DE CALIDAD \n" +
+                "Piezas aprobadas " + aprobadas + "\n" +
+                "Piezas defectuosas " + defectuosas;
+
+        JOptionPane.showMessageDialog(null, resultado);
+    }
+}
