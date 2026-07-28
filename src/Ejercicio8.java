ef05887ff63ac165f6c5b37a91a3de7b30e7c9a4
camila <urreacamila@gmail.com>
2026-07-27 21:39:29 -0500

    feat: resuelve ejercicio 8 - inversor cadena

---
+++ src/Ejercicio8.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        String palabra;
+        String invertida = "";
+
+        palabra = JOptionPane.showInputDialog("ingrese una palabra:");
+
+        for (int i = palabra.length() -1; i >= 0; i--){
+            invertida = invertida + palabra.charAt(i);
+        }
+        JOptionPane.showMessageDialog(null, "cadena invertida:" + invertida);
+    }
+}
