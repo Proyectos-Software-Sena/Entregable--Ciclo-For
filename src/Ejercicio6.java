568a9a1a34e67f698038c10f6bc71b10b62dfc8d
Steven <steven2008hernandez798@gmail.com>
2026-07-27 20:43:36 -0500

    Corregí ejercicio 6 ciclo for

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ejercicio6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Cantidad de asistentes: ");
+        int asistentes = sc.nextInt();
+
+        int menores = 0;
+        int adultos = 0;
+
+        for (int i = 1; i <= asistentes; i++) {
+
+            System.out.print("Edad del asistente " + i + ": ");
+            int edad = sc.nextInt();
+
+            if (edad < 18) {
+                menores++;
+            } else {
+                adultos++;
+            }
+        }
+
+        System.out.println("Menores: " + menores);
+        System.out.println("Adultos: " + adultos);
+
+        sc.close();
+    }
 }
