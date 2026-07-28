e7272a0d4f5092355a263bd29a50196fc4d2ea27
Steven <steven2008hernandez798@gmail.com>
2026-07-27 20:49:00 -0500

    Agregué ejercicio 8 ciclo for

---
+++ src/Ejercicio8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese una palabra: ");
+        String palabra = sc.nextLine();
+
+        String invertida = "";
+
+        for (int i = palabra.length() - 1; i >= 0; i--) {
+
+            invertida += palabra.charAt(i);
+        }
+
+        System.out.println("Cadena invertida: " + invertida);
+
+        sc.close();
+    }
+}
