6ec0e4f0c8e74cf9264d0f0f6ec73393cdf9d18d
juliana parra <juliparra481@gmail.com>
2026-07-27 21:09:35 -0500

    guarde el ejercicio9

---
+++ Ciclo For/src/Ejercicio9.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Ejercicio9 {
+    public static void main(String[] args){
+
+        int bacterias = Integer.parseInt(JOptionPane.showInputDialog("cuantas bacterias va a ingresar? "));
+        int horas = Integer.parseInt(JOptionPane.showInputDialog("durante cuantas horas? "));
+
+        for (int i = 1;i <= horas;i++){
+            int crecimiento = bacterias * 2;
+            bacterias = crecimiento;
+            System.out.println("Hora: " + i + ": " +  crecimiento + " Bacterias: ");
+        }
+    }
+}
