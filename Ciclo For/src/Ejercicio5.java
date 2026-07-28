8dcffd636e06b3716c6d647934356bcc224e7488
juliana parra <juliparra481@gmail.com>
2026-07-27 19:27:32 -0500

    guarde el ejercicio5

--- Ciclo For/src/Ejercicio5.java
+++ Ciclo For/src/Ejercicio5.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
 public class Ejercicio5 {
+    public static void main (String[] args){
+        int ahorrar = Integer.parseInt(JOptionPane.showInputDialog("ingrese la cantidad de dinero que quiere ahorar"));
+        int meses = Integer.parseInt(JOptionPane.showInputDialog("ingrese los meses en los que va a ahorrar"));
+        int total = 0;
+        for(int i = 1; i <= meses; i++ ){
+            total = ahorrar * i;
+            System.out.println("el ahorrro es de: " + i + "$" + total);
+        }
+
+
+
+    }
 }
