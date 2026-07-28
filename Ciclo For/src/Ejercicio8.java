09736d4d8e8748f325e4cc4e8ca804c309eeb0cb
juliana parra <juliparra481@gmail.com>
2026-07-27 20:54:14 -0500

    guarde el ejercicio8

---
+++ Ciclo For/src/Ejercicio8.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class Ejercicio8 {
+    public static void main(String[] args){
+
+        String palabra = JOptionPane.showInputDialog("ingresa la palabra clave: ");
+        String clave = "";
+        for (int i = palabra.length() - 1;i >= 0; i--){
+            clave += palabra.charAt(i);
+        }
+        JOptionPane.showMessageDialog(null,"la clave es: " + clave);
+    }
+}
